eabd7fe2f77005cff4dda10f59141725321771eb
Diego Espelho <diego.espelho@gmail.com>
2024-12-19 23:10:01 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 import plugin from 'tailwindcss/plugin';
 
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/index.html', './src/**/*.{html,js}'],
   theme: {
     container: {
       center: true,
